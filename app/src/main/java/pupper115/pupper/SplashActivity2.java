a31d516942f94ae79cbbc43d89ca9ab25b495ad0
Aaron Le <aadle@ucsc.edu>
2017-10-25 16:21:20 -0700

    removed splashactivity.java. caused errors. splashactivity2 connects to aws

---
+++ app/src/main/java/pupper115/pupper/SplashActivity2.java
@@ -0,0 +1,37 @@
+package pupper115.pupper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
+import com.amazonaws.mobile.auth.core.StartupAuthResult;
+import android.content.Intent;
+import android.content.Context;
+
+public class SplashActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+        Context appContext = getApplicationContext();
+        AWSConfiguration awsConfig = new AWSConfiguration(appContext);
+        IdentityManager identityManager = new IdentityManager(appContext, awsConfig);
+        IdentityManager.setDefaultIdentityManager(identityManager);
+        identityManager.doStartupAuth(this, new StartupAuthResultHandler() {
+            @Override
+            public void onComplete(StartupAuthResult startupAuthResult) {
+                // User identity is ready as unauthenticated user or previously signed-in user.
+            }
+        });
+
+        // Go to the main activity
+        final Intent intent = new Intent(this, LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        this.startActivity(intent);
+        this.finish();
+
+    }
+}
