a090a565b8736827cf5d1e25ea2557b6ede358de
JPL4494 <joshlong4494@gmail.com>
2017-11-16 12:56:38 -0800

    Can pour to bucket
    The code to put something in the bucket now works

--- app/src/main/java/pupper115/pupper/CreateDogProfile.java
+++ app/src/main/java/pupper115/pupper/CreateDogProfile.java
@@ -1,32 +1,43 @@
 package pupper115.pupper;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferType;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import pupper115.pupper.s3bucket.Constants;
+import pupper115.pupper.s3bucket.Util;
 
 /**
- * Created by Josh on 11/8/2017.
+ * This file was altered from the amazon aws github for uploading to the S3 buckets
+ * It took a lot to get it working to this point
  */
-
 public class CreateDogProfile extends AppCompatActivity {
     private EditText name = null;
     private EditText age = null;
@@ -36,8 +47,32 @@ public class CreateDogProfile extends AppCompatActivity {
     private String dogAge = "";
     private String dogBio = "";
 
-    private Bitmap dogPicture = null;
-    private String filePath = "";
+    private static final int REQUEST_WRITE_PERMISSION = 786;
+
+    // Indicates that no upload is currently selected
+    private static final int INDEX_NOT_CHECKED = -1;
+
+    // TAG for logging;
+    private static final String TAG = "UploadActivity";
+
+    // The TransferUtility is the primary class for managing transfer to S3
+    private TransferUtility transferUtility;
+
+    // The SimpleAdapter adapts the data about transfers to rows in the UI
+    private SimpleAdapter simpleAdapter;
+
+    // A List of all transfers
+    private List<TransferObserver> observers;
+
+    /**
+     * This map is used to provide data to the SimpleAdapter above. See the
+     * fillMap() function for how it relates observers to rows in the displayed
+     * activity.
+     */
+    private ArrayList<HashMap<String, Object>> transferRecordMaps;
+
+    // Which row in the UI is currently checked (if any)
+    private int checkedIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,32 +81,80 @@ public class CreateDogProfile extends AppCompatActivity {
 
         EditText focus = (EditText) findViewById(R.id.name);
         focus.requestFocus();
+
+        // Initializes TransferUtility, always do this before using it.
+        transferUtility = Util.getTransferUtility(this);
+        checkedIndex = INDEX_NOT_CHECKED;
+        transferRecordMaps = new ArrayList<HashMap<String, Object>>();
+        requestPermission();
     }
 
-    public void registerDog()
-    {
-        Intent data = getIntent();
-        String username = data.getStringExtra("userName");
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Get the data from any transfer's that have already happened,
+        initData();
+    }
 
-        String bucketName = "http://s3.amazonaws.com/pupper-user-info/";
-        String keyName = username;
-
-        CheckBox checked = (CheckBox) findViewById(R.id.checkBox);
-        Boolean isUpForAdoption = checked.isChecked();
-
-        File dogFile = new File(filePath);
-
-        AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider());
-        try {
-            Log.d("Pre:", "Entering");
-            s3client.putObject(new PutObjectRequest(bucketName, keyName, dogFile));
-            Log.d("Pre:", "Finished");
-        } catch (AmazonServiceException e) {
-            Log.d("Result:", "CRASHED HERE");
-            System.err.println(e.getErrorMessage());
-            System.exit(1);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Clear transfer listeners to prevent memory leak, or
+        // else this activity won't be garbage collected.
+        if (observers != null && !observers.isEmpty()) {
+            for (TransferObserver observer : observers) {
+                observer.cleanTransferListener();
+            }
         }
+    }
 
+    /**
+     * Gets all relevant transfers from the Transfer Service for populating the
+     * UI
+     */
+    private void initData() {
+        transferRecordMaps.clear();
+        // Use TransferUtility to get all upload transfers.
+        observers = transferUtility.getTransfersWithType(TransferType.UPLOAD);
+        TransferListener listener = new UploadListener();
+        for (TransferObserver observer : observers) {
+
+            // For each transfer we will will create an entry in
+            // transferRecordMaps which will display
+            // as a single row in the UI
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            Util.fillMap(map, observer, false);
+            transferRecordMaps.add(map);
+
+            // Sets listeners to in progress transfers
+            if (TransferState.WAITING.equals(observer.getState())
+                    || TransferState.WAITING_FOR_NETWORK.equals(observer.getState())
+                    || TransferState.IN_PROGRESS.equals(observer.getState())) {
+                observer.setTransferListener(listener);
+            }
+        }
+        //simpleAdapter.notifyDataSetChanged();
+    }
+
+    /*
+     * Updates the ListView according to the observers.
+     */
+    private void updateList() {
+        TransferObserver observer = null;
+        HashMap<String, Object> map = null;
+        for (int i = 0; i < observers.size(); i++) {
+            observer = observers.get(i);
+            map = transferRecordMaps.get(i);
+            Util.fillMap(map, observer, i == checkedIndex);
+        }
+        //simpleAdapter.notifyDataSetChanged();
+
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        }
     }
 
     public void checkResponse(View v)
@@ -108,22 +191,156 @@ public class CreateDogProfile extends AppCompatActivity {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == 1) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
 
-            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-            cursor.moveToFirst();
+            try {
+                String path = getPath(uri);
+                beginUpload(path);
+            } catch (URISyntaxException e) {
+                Toast.makeText(this,
+                        "Unable to get the file from the given URI.  See error log for details",
+                        Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Unable to upload file from the given uri", e);
+            }
+        }
+    }
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            filePath = cursor.getString(columnIndex);
-            cursor.close();
+    /*
+     * Begins to upload the file specified by the file path.
+     */
+    private void beginUpload(String filePath) {
+        if (filePath == null) {
+            Toast.makeText(this, "Could not find the filepath of the selected file",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        File file = new File(filePath);
+        TransferObserver observer = transferUtility.upload(Constants.BUCKET_NAME, file.getName(),
+                file);
+        /*
+         * Note that usually we set the transfer listener after initializing the
+         * transfer. However it isn't required in this sample app. The flow is
+         * click upload button -> start an activity for image selection
+         * startActivityForResult -> onActivityResult -> beginUpload -> onResume
+         * -> set listeners to in progress transfers.
+         */
+        // observer.setTransferListener(new UploadListener());
+    }
 
-            dogPicture = BitmapFactory.decodeFile(filePath);
+    /*
+     * Gets the file path of the given Uri.
+     */
+    @SuppressLint("NewApi")
+    private String getPath(Uri uri) throws URISyntaxException {
+        final boolean needToCheckUri = Build.VERSION.SDK_INT >= 19;
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        // deal with different Uris.
+        if (needToCheckUri && DocumentsContract.isDocumentUri(getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("image".equals(type)) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[] {
+                        split[1]
+                };
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {
+                    MediaStore.Images.Media.DATA
+            };
+            Cursor cursor = null;
+            try {
+                cursor = getContentResolver()
+                        .query(uri, projection, selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
 
-            registerDog();
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /*
+     * A TransferListener class that can listen to a upload task and be notified
+     * when the status changes.
+     */
+    private class UploadListener implements TransferListener {
+
+        // Simply updates the UI list when notified.
+        @Override
+        public void onError(int id, Exception e) {
+            Log.e(TAG, "Error during upload: " + id, e);
+            updateList();
+        }
+
+        @Override
+        public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+            Log.d(TAG, String.format("onProgressChanged: %d, total: %d, current: %d",
+                    id, bytesTotal, bytesCurrent));
+            updateList();
+        }
+
+        @Override
+        public void onStateChanged(int id, TransferState newState) {
+            Log.d(TAG, "onStateChanged: " + id + ", " + newState);
+            updateList();
+
+            if(newState == TransferState.COMPLETED)
+            {
+                Log.d("Picture:", "The picture was poured into the bucket!");
+                Intent resultIntent = new Intent();
+                setResult(2, resultIntent);
+                finish();
+            }
         }
     }
 }
